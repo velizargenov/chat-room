71218dadab9f1bae41f67283d8c5f318aa77a975
Velizar Genov <velizar.genov@gmail.com>
2015-11-04 00:47:51 +0000

    Chat room

--- app/app.js
+++ app/app.js
@@ -2,15 +2,52 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var nicknames = [];
 
-server.listen(8080);
+// Route to
 app.use(express.static(__dirname + '/client'));
 
+// Receive message
 io.on('connection', function (socket) {
-  console.log('connected');
-  socket.emit('message', 'hello');
-  socket.on('message', function (message) {
-    console.log(message)
-    socket.broadcast.emit('message', message);
+  socket.on('new user', function (data, callback) {
+    if (nicknames.indexOf(data) != -1) {
+      callback(false); // callback({isValid: false})
+    }
+    else {
+      callback(true);
+      socket.nickname = data;                                                     // store each nickname to the socket
+      nicknames.push(socket.nickname);
+      updateNicknames();
+    }
   });
+
+  console.log('a user connected');
+  socket.emit('message', 'hello');
+
+  // Send message
+  socket.on('message', function (data) {
+    console.log('message: ' + data)
+    socket.broadcast.emit('message', data);
+    // socket.emit('message', {msg: data, nick: socket.nickname})
+    // socket.broadcast.emit('message', {msg: data, nick: socket.nickname});
+  });
+
+  // Update nicknames
+  function updateNicknames() {
+    io.sockets.emit('usernames', nicknames);
+  }
+
+  // Disconnect
+  socket.on('disconnect', function (data) {
+    console.log('user disconnected');
+    if (!socket.nickname) return;
+    nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+    updateNicknames();
+  });
+});
+
+
+// Server Listen
+server.listen(8080, function() {
+  console.log('listening on port: 8080');
 });
--- app/client/client.js
+++ app/client/client.js
@@ -1,9 +1,80 @@
 var socket = io.connect('http://localhost:8080');
+var $message = $('#msg');
+var $messages = $('#messages');
+var $nickForm = $("#set-username");
+var $nickError = $(".error");
+var $nickBox = $("#nickname");
+var $users = $(".users");
 
-socket.on('message', function (data) {
+
+socket.on('message', function (data, username) {
   console.log(data);
+  console.log("username: ", username)
 });
 
-function send (message) {
-  socket.emit('message', message);
+// Set username
+$("#submit-name").on("click", function () {
+  socket.emit('new user', $nickBox.val(), function (data) {
+    if (data) {
+      $('.details-wrapper').hide();
+      $(".content-wrapper, .user-summery").addClass("content-show");
+    }
+    else {
+      $nickError.html('The username is already taken, please try again');
+    }
+  });
+  $nickBox.val("");
+});
+
+
+// Display online users
+socket.on('usernames', function (data) {
+  var names = '';
+  for (var i = 0; i < data.length; i++) {
+    names += data[i] + '<br/>';
+  }
+  $users.html(names);
+});
+
+
+// Submit message
+$('.chat-wrapper').find('form').submit(function () {
+  var val = $message.val();
+  socket.emit('message', val);
+  appendMessage(val);
+  $message.val('');
+  return false;
+});
+
+
+// Send message
+socket.on('message', appendMessage);
+function appendMessage(data) {
+  $messages.append($('<li>').text(data));
+  console.log('nick: ', data.nick);
+  console.log('msg: ', data.msg);
+  // $messages.append('<b>' + data.nick + ': </b>' + data.msg);
+}
+
+
+// Minimize chat box
+$(document).on("click", ".chat-close", function() {
+  $(".chat-wrapper").hide();
+});
+
+var $height = $(".chat-wrapper").height();
+
+if ($height > 50) {
+  $(document).on("click", ".chat-minimize", function() {
+    console.log('1', $height)
+    $("form").css('position', 'relative');
+    $(".chat-wrapper").animate({height:30}, 100);
+  });
+}
+
+if ($height < 50) {
+  $(document).on("click", ".chat-minimize", function() {
+    console.log('2', $height)
+    $(".chat-wrapper").hide()
+  });
 }
